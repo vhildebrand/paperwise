f488818ae103ea381bfa374f9e9eb4e6a8366535
vhildebrnad <dv_hildebrand@proton.me>
2025-06-17 12:14:01 -0400

    fix: fix linting errors

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../store/auth';
 import { IconFile } from '../assets/Icons';
 
